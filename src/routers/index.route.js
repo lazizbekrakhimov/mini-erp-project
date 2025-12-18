2df89854812114152142b265a703aa3dd950d448
jurambekovs-ui <jurambekovs@gmail.com>
2025-12-18 23:59:30 +0500

    fix

--- src/routers/index.route.js
+++ src/routers/index.route.js
@@ -3,21 +3,11 @@ import { Router } from "express";
 import adminRouter from "../routers/admin.route.js";
 import categoryRouter from "../routers/category.route.js";
 import productRouter from "../routers/product.route.js";
-<<<<<<< HEAD
-=======
-import stockRouter from "../routers/stock.route.js";
-import supplierRoute from "../routers/supplier.route.js";
->>>>>>> 96910b0 (supplier)
 
 const router = Router();
 
 router.use("/category", categoryRouter);
 router.use("/products", productRouter);
 router.use("/admin", adminRouter);
-<<<<<<< HEAD
-=======
-router.use("/stock", stockRouter);
-router.use("/suppliers", supplierRoute);
->>>>>>> 96910b0 (supplier)
 
 export default router
